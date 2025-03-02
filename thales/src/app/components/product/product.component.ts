e0dff592c62aaff74c3d151100451873a06cb9b6
juan leon <leonjuandavid@hotmail.com>
2025-03-02 14:14:03 -0500

    Ajsutes visuales

--- thales/src/app/components/product/product.component.ts
+++ thales/src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
@@ -9,11 +9,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTableModule} from '@angular/material/table';
 import {MatDialog} from '@angular/material/dialog';
 import { DialogModalComponent } from '../dialog-modal/dialog-modal.component';
+import {MatCardModule} from '@angular/material/card';
+
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-product',
   standalone: true, 
-  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule,MatTableModule], 
+  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule,MatTableModule, MatCardModule], 
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
@@ -24,7 +28,7 @@ export class ProductComponent {
   
   readonly dialog = inject(MatDialog);
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, @Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
@@ -49,10 +53,16 @@ export class ProductComponent {
             data: { message: JSON.stringify(err.error) } // Convierte a string
           });
           this.products = [];
+          this.productId = null;
         }
       });
     } else {
       this.getAllProducts();
     }
+  }
+
+  onImageError(event: Event) {
+    console.log(this.document.location.origin);
+    (event.target as HTMLImageElement).src = 'https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg';
   }
 }
