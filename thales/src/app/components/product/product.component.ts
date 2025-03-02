97aefd846d661ea3f19005b52f5a68af6699b7f7
juan leon <leonjuandavid@hotmail.com>
2025-03-02 14:44:42 -0500

    fixes

--- thales/src/app/components/product/product.component.ts
+++ thales/src/app/components/product/product.component.ts
@@ -62,7 +62,6 @@ export class ProductComponent {
   }
 
   onImageError(event: Event) {
-    console.log(this.document.location.origin);
     (event.target as HTMLImageElement).src = 'https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg';
   }
 }
